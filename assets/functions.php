c9d3bcfc57637bed4f8db0e205998f20cd066cd2
Danilo <44043259+Danilo1990@users.noreply.github.com>
2025-07-31 17:56:10 +0200

    Test materialize

--- assets/functions.php
+++ assets/functions.php
@@ -1,41 +1,27 @@
 <?php
 
-function renderPage($title = '') {
+function renderPage($title) {
     require 'assets/db.php';
     echo '<!DOCTYPE html>';
     echo '<html lang="it">';
+    
     echo '<head>';
         echo '<meta charset="UTF-8">';
         echo '<meta name="viewport" content="width=device-width, initial-scale=1.0">';
         echo '<title>' . htmlspecialchars($title) . '</title>';  
-        injectScripts();
-    echo '</head>';          
+       
+        echo '<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>';  
+        echo '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">';
+        echo '<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>';     
+        echo '<script src="/main.js"></script>'; 
+        echo '<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">'; 
+    
+    echo '</head>';   
+
     echo '<body>';
-    navbar();
 }
 
 function footerPage() {
-    echo '</body>';
+        echo '</body>';
     echo '</html>';
-}
-
-function injectScripts() {
-    echo '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">';
-    echo '<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>';     
-}
-
-function navbar() {
-    echo '<div class="navbar-fixed">';
-        echo '<nav>';
-        echo '<div class="container">';
-                echo '<div class="nav-wrapper">';
-                    echo '<a href="#!" class="brand-logo">Logo</a>';
-                    echo '<ul class="right hide-on-med-and-down">';
-                        echo '<li><a href="sass.html">Sass</a></li>';
-                        echo '<li><a href="badges.html">Components</a></li>';
-                    echo '</ul>';
-                echo '</div>';
-            echo '</div>';
-        echo '</nav>';
-    echo '</div>';
 }
